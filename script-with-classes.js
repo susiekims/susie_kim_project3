ff9a7d6d7fffb5ae48bd6969bf75ac3ad739de1c
Susie <susie.kim@hotmail.com>
2018-08-15 16:36:03 -0400

    device test

--- script-with-classes.js
+++ script-with-classes.js
@@ -1,23 +1,23 @@
 // create game object for functions to live on
 const game = {};
 
-// declare score, update score dynamically 
-game.score = 0;
-game.lives = 3;
-game.speed = 2000;
-game.time = 20000;
-game.numberOfBalls = game.time / 1000;
-game.ballTypes = [
-    'assets/hair.png',
-    'assets/hilaryfixed.png'
-];
+
+game.playSound = (soundName) => {
+    game[soundName] = document.createElement('audio');
+    game[soundName].src = `assets/${soundName}.mp3`;
+    game[soundName].volume = 0.8;
+    game[soundName].autoPlay = false;
+    game[soundName].preLoad = true;
+    game[soundName].controls = true;
+    game[soundName].play();
+}
 
 let randomNum = Math.floor(Math.random() * 2);
 
-game.getScreenWidth = () => {
-    const stageWidth = $(window).width() / 2;
-    $('.stage').css('width', stageWidth);
-}
+// game.getScreenWidth = () => {
+//     const stageWidth = $(window).width() / 2;
+//     $('.stage').css('width', stageWidth);
+// }
 
 game.startTimer = () => {
     $('#timer').text(game.time /1000 + ':00');
@@ -27,8 +27,8 @@ game.startTimer = () => {
         if (game.time == 0) {
             clearInterval(timer);
             console.log(game.score);
-            $('#score').text(game.score);
-            $('#finish-screen').toggle(true);
+            $('#score-final').append(game.score);
+            $('#finish-screen').css('opacity', 1);
             $('#replay').on('click', function(){
                 window.location.reload();
             });
@@ -53,8 +53,6 @@ game.moveTrump = () => {
     const trump = $('#trump');
     // let trumpPositionX = trump.position().left;
      let trumpPositionX = game.interval;
-
-
     // using arrow keys
     $("body").keydown(function(e){
         // console.log(game.interval);
@@ -83,7 +81,6 @@ game.moveTrump = () => {
             trump.css('left', trumpPositionX);
 
         } else if ((e.keyCode || e.which) == 39 && trumpPositionX == game.interval * 2) {
-            console.log('ouch!');
             trumpPositionX = game.interval * 2;
             trump.css('left', trumpPositionX);
         }
@@ -130,7 +127,7 @@ game.moveBall = (index) => {
     const $ball = $(`#ball${index}`);
     $ball.css('left', (Math.floor(Math.random() * 3))*game.interval );
     $ball.animate({
-        top: "+=700px"
+        top: $('.stage').height() + game.interval
       }, game.speed);
     game.checkPosition(index);
 
@@ -142,7 +139,7 @@ game.moveBall = (index) => {
 // compare y positions of ball and box within 40px
 // if they touch, add 1 to the score and delete the ball
 game.checkPosition = (index) => {
-    const stopCheck = setInterval(function(){
+    const check = setInterval(function(){
         const $ball = $(`#ball${index}`);
         const ballPositionY = $ball.position().top;
         const ballPositionX = $ball.position().left;
@@ -157,10 +154,11 @@ game.checkPosition = (index) => {
             && ballPositionY < trumpPositionY - 20
             && trumpPositionX == ballPositionX 
             && $ball.attr('src') == 'assets/hair.png') {
+            game.playSound('chaching');
             $ball.remove();
             game.score++;
             $('#score span').text(game.score);
-            clearInterval(stopCheck);
+            clearInterval(check);
             trump.attr('src', 'assets/trumpsmile.png');
             setTimeout(()=>{
                 trump.attr('src', 'assets/trump.png');
@@ -173,20 +171,23 @@ game.checkPosition = (index) => {
             game.lives--;
             $('#lives span').text(game.lives);
             $ball.remove();
-            clearInterval(stopCheck);
+            clearInterval(check);
             trump.attr('src', 'assets/trump-angry.png');
             setTimeout(()=>{
                 trump.attr('src', 'assets/trump.png');
             }, 500);
             if (game.lives === 0) {
-                clearInterval(stopCheck);
+                clearInterval(check);
                 if (alert('game over! play again?')){} else {window.location.reload()} 
             }
+        } else if (ballPositionY > $('.stage').height()) {
+            clearInterval(check);
+            $ball.remove();
         }
     }, 50);
 }
 
-game.init = () => {
+game.playGame = () => {
     game.moveTrump();
     game.startTimer();
     for (let i = 0; i < game.numberOfBalls; i++) {
@@ -197,21 +198,35 @@ game.init = () => {
     }  
 }
 
-$(function() {
-    console.log("ready!");
-    game.getScreenWidth();
+game.init = () => {
+    // declare score, update score dynamically 
+    game.score = 0;
+    game.lives = 3;
+    game.speed = 2000;
+    game.time = 20000;
+    game.numberOfBalls = game.time / 1000;
+    game.ballTypes = [
+        'assets/hair.png',
+        'assets/hilaryfixed.png'
+    ];
+    // game.getScreenWidth();
     game.responsiveResize();
     $('#loading-screen').toggle(true);
-    $('#finish-screen').toggle(false);
+    $('#finish-screen').css('opacity', 0);
     $('#start').on('click', function(){
         $('#loading-screen').toggle(false);
         $('#score span').text(game.score);
         $('#lives span').text(game.lives);
         $('#start').remove();
-        game.init();  
+        game.playGame();  
     });
     $(window).resize(function(){
         game.responsiveResize();
     });
+}
+
+$(function() {
+    console.log("ready!");
+    game.init();
 });
 
